2b646abdb9f9da5367618d397a249c8a82988a74
Learn AI <160322436+WahZah@users.noreply.github.com>
2024-02-22 21:15:06 +0500

    Add files via upload

---
+++ Ex-02.ts
@@ -0,0 +1,4 @@
+// Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?
+
+let nameOfPerson : string = "Eric";
+console.log(`Hello ${nameOfPerson},  would you like to learn some Python today?`);
